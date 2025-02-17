851d176087f0bb6ed123a85043b0aea688e44f93
cometagentai <cometagentai@gmail.com>
2025-02-17 21:04:30 +0530

    fix: update

--- app/(app)/bridge/config/constants.ts
+++ app/(app)/bridge/config/constants.ts
@@ -2,9 +2,9 @@ import { type Chain } from '@wormhole-foundation/sdk';
 
 export const WORMSCAN = 'https://wormholescan.io/#/';
 
-export const CONNECT_VERSION = '1.0.0';
+export const CONNECT_VERSION = '2.1.0-beta.0-development';
 
-export const CONNECT_GIT_HASH = 'unknown';
+export const CONNECT_GIT_HASH = '4885598ba2c8105a4eec835cf7cfa1ae19a3d19c';
 
 export const CHAIN_ORDER: Chain[] = [
   'Ethereum',
